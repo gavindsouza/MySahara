786c5c7e89af108e9c7beda26f641c64f3bad2d9
blacklegchef <gavin18d@gmail.com>
2018-02-28 16:54:34 +0530

    commit #1

---
+++ app/src/main/java/com/sfit/sahara/mysahara/MainActivity.java
@@ -0,0 +1,23 @@
+package com.sfit.sahara.mysahara;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button yourButton = (Button) findViewById(R.id.buttons_id);
+        yourButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, NormalActivity.class));
+            }
+        });
+    }
+}
+
