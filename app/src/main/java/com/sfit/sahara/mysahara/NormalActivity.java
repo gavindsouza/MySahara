161143b1ea946a0f89be7e277056104b28010d5d
blacklegchef <gavin18d@gmail.com>
2018-02-28 21:48:15 +0530

    user view edit #2

--- app/src/main/java/com/sfit/sahara/mysahara/NormalActivity.java
+++ app/src/main/java/com/sfit/sahara/mysahara/NormalActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class NormalActivity extends AppCompatActivity {
@@ -13,8 +12,6 @@ public class NormalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
