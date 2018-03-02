7fb6cd2b4f850b1f24e9608e641e6aed8ffdbef5
blacklegchef <gavin18d@gmail.com>
2018-03-02 11:21:35 +0530

    test #4

--- app/src/main/java/com/sfit/sahara/mysahara/NormalActivity.java
+++ app/src/main/java/com/sfit/sahara/mysahara/NormalActivity.java
@@ -1,10 +1,10 @@
 package com.sfit.sahara.mysahara;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class NormalActivity extends AppCompatActivity {
 
@@ -13,12 +13,10 @@ public class NormalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Button yourButton = (Button) findViewById(R.id.log_out);
+        yourButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent( NormalActivity.this, MainActivity.class));
             }
         });
     }
