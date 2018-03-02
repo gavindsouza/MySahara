f9070610edb3954c78816f592c7892e41e690543
Shwetadsa <swetli0710@gmail.com>
2018-03-02 13:40:51 +0530

    old person

---
+++ app/src/main/java/com/sfit/sahara/mysahara/oldPersonHome.java
@@ -0,0 +1,15 @@
+package com.sfit.sahara.mysahara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class oldPersonHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_old_person_home);
+
+        //fetch name of old man from database and dispplay it
+    }
+}
