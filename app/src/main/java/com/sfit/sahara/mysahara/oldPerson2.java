50a1650aabac8cdbe4a6821933a9429bca8b7921
Shwetadsa <swetli0710@gmail.com>
2018-03-02 13:40:51 +0530

    old person

---
+++ app/src/main/java/com/sfit/sahara/mysahara/oldPerson2.java
@@ -0,0 +1,13 @@
+package com.sfit.sahara.mysahara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class oldPerson2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_old_person2);
+    }
+}
