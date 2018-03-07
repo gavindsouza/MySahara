b277522aee32987399e679755bce019afef958cd
Shwetadsa <swetli0710@gmail.com>
2018-03-07 14:26:57 +0530

    signup

--- app/src/main/java/com/sfit/sahara/mysahara/junior_second_page__signup.java
+++ app/src/main/java/com/sfit/sahara/mysahara/junior_second_page__signup.java
@@ -5,19 +5,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class junior_second_page__signup extends Activity {
-    @Override
+    EditText etFname, etLname, etContact, etUsername, etPass, etConfirmPass;
+    Button btnSignup;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.junior_second_page__signup);
-        Button sback = (Button) findViewById(R.id.back);
-        sback.setOnClickListener(new View.OnClickListener() {
+
+        etFname = findViewById(R.id.etFname);
+        etLname = findViewById(R.id.etLname);
+        etContact = findViewById(R.id.etContact);
+        etUsername = findViewById(R.id.etUsername);
+        etPass = findViewById(R.id.etPass);
+        etConfirmPass = findViewById(R.id.etConfirmPass);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(junior_second_page__signup.this,junior_second_page_login.class));
+
             }
         });
-    }
 
+    }
 }
