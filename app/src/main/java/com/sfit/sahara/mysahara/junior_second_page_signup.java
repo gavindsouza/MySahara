345b70c43f5871c8dffb5ea2a718c7c2b9a770c5
blacklegchef <gavin18d@gmail.com>
2018-03-07 19:57:32 +0530

    need fix junior_second_page_signup crash

--- app/src/main/java/com/sfit/sahara/mysahara/junior_second_page_signup.java
+++ app/src/main/java/com/sfit/sahara/mysahara/junior_second_page_signup.java
@@ -2,6 +2,8 @@ package com.sfit.sahara.mysahara;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,34 +13,55 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class junior_second_page_signup extends Activity {
-    EditText etFname, etLname, etContact, etUsername, etPass, etConfirmPass;
-    Button btnSignup =(Button) findViewById(R.id.btnSignup);
     FirebaseFirestore db;
+    final EditText fname = (EditText) findViewById(R.id.etFname);
+    final EditText lname = (EditText) findViewById(R.id.etLname);
+    final EditText contact = (EditText) findViewById(R.id.etContact);
+    final EditText username = (EditText) findViewById(R.id.etUsername);
+    final EditText password = (EditText) findViewById(R.id.etPass);
+    final EditText confirm = (EditText) findViewById(R.id.etConfirmPass);
+    final Button signup =(Button) findViewById(R.id.btnSignup);
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.junior_second_page_signup);
 
-        etFname = findViewById(R.id.etFname);
-        etLname = findViewById(R.id.etLname);
-        etContact = findViewById(R.id.etContact);
-        etUsername = findViewById(R.id.etUsername);
-        etPass = findViewById(R.id.etPass);
-        etConfirmPass = findViewById(R.id.etConfirmPass);
+        final String f_name = fname.getText().toString();
+        final String l_name = lname.getText().toString();
+        final String contact_num = contact.getText().toString();
+        final String user = username.getText().toString();
+        final String pass = password.getText().toString();
+        final String confirm_pass = confirm.getText().toString();
 
-       if((etFname!=null)&&(etContact!=null)&&(etUsername!=null)&&(etPass!=null)&&(etConfirmPass!=null))
-            btnSignup.setEnabled(true);
-        btnSignup.setOnClickListener(new View.OnClickListener() {
+        signup.setEnabled(false);
+        //if((f_name!=null)&&(contact_num!=null)&&(username!=null)&&(password!=null)&&(confirm!=null))
+
+        confirm.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                Map<String, Object> m=new HashMap<>();
-                m.put("First Name",etFname);
-                m.put("Last Name",etLname);
-                m.put("First Name",etFname);
-
-                db.collection("sahara").document("users").set(m);
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                    if(pass.equals(confirm_pass))
+                        signup.setEnabled(true);
             }
         });
 
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, Object> m=new HashMap<>();
+                m.put("First Name",f_name);
+                m.put("Last Name",l_name);
+                m.put("Contact",contact_num);
+                m.put("Username",user);
+                m.put("Password",pass);
+                db.collection("sahara").document("users").set(m);
+            }
+        });
     }
 }
