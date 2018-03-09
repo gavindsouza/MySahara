91f83be2356069366ab812a19820722cec67b61e
blacklegchef <gavin18d@gmail.com>
2018-03-10 01:01:42 +0530

    added firestore signup

--- app/src/main/java/com/sfit/sahara/mysahara/MainActivity.java
+++ app/src/main/java/com/sfit/sahara/mysahara/MainActivity.java
@@ -1,23 +1,65 @@
 package com.sfit.sahara.mysahara;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db;
+    String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button yourButton = (Button) findViewById(R.id.buttons_id);
+        final Button yourButton = (Button) findViewById(R.id.buttons_id);
         yourButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startActivity(new Intent(MainActivity.this, junior_fifth_page_home.class));
-            }
-        });
+                public void onClick(View view) {
+                    db=FirebaseFirestore.getInstance();
+                    Map<String, Object> m = new HashMap<>();
+                    m.put("First Name", "f_name");
+                    m.put("Last Name", "l_name");
+                    m.put("Contact", "contact_num");
+                    m.put("Username", "user");
+                    m.put("Password", "pass");
+
+                    Map<String, Object> city = new HashMap<>();
+                    city.put("name", "Los Angeles");
+                    city.put("state", "CA");
+                    city.put("country", "USA");
+
+                    try {
+                        db.collection("sahara").document("users").set(m);
+                    db.collection("cities").document("LA")
+                            .set(city)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "DocumentSnapshot successfully written!");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error writing document", e);
+                                }
+                            });
+                    }
+                    catch (Exception e){
+                        yourButton.setText("Failed");
+                    }
+                }
+            });
     }
 }
 
--- app/src/main/java/com/sfit/sahara/mysahara/junior_second_page_login.java
+++ app/src/main/java/com/sfit/sahara/mysahara/junior_second_page_login.java
@@ -5,18 +5,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class junior_second_page_login extends Activity {
-
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.junior_second_page_login);
-        Button sup = (Button) findViewById(R.id.signup);
-        sup.setOnClickListener(new View.OnClickListener() {
+
+        Button login = (Button)findViewById(R.id.login);
+        Button signup = (Button) findViewById(R.id.signup);
+        EditText username=(EditText)findViewById(R.id.username);
+        EditText password=(EditText)findViewById(R.id.password);
+        final String user=username.getText().toString();
+        final String pass=password.getText().toString();
+
+        signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(junior_second_page_login.this,junior_second_page_signup.class));
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db=FirebaseFirestore.getInstance();
+                db.collection("sahara").document("users").collection(user).document("Data").getFirestore();
             }
         });
     }
