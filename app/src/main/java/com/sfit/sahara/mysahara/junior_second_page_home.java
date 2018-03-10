f3f801683b404405c100c88475f35a0e3c8f291a
blacklegchef <gavin18d@gmail.com>
2018-03-10 09:14:57 +0530

    login fix needed

--- app/src/main/java/com/sfit/sahara/mysahara/junior_second_page_home.java
+++ app/src/main/java/com/sfit/sahara/mysahara/junior_second_page_home.java
@@ -1,7 +1,10 @@
 package com.sfit.sahara.mysahara;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class junior_second_page_home extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class junior_second_page_home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.junior_second_page_home);
+        Button b=findViewById(R.id.changescreen);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(junior_second_page_home.this,junior_third_page_details.class));
+            }
+        });
     }
 }
