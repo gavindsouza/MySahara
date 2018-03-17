23d37ff3e3423914c01fba3e71856197b19f5ac5
Marica <maricadsouza221197@gmail.com>
2018-03-17 16:05:03 +0530

    Notification functional but only for onclick for now(tap on the notif lol)

--- app/src/main/java/com/sfit/sahara/mysahara/junior_fifth_page_home.java
+++ app/src/main/java/com/sfit/sahara/mysahara/junior_fifth_page_home.java
@@ -1,5 +1,6 @@
 package com.sfit.sahara.mysahara;
 
+import android.app.Notification;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,18 +13,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class junior_fifth_page_home extends AppCompatActivity {
-
+   NotificationHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.junior_fifth_page_home);
-
+        helper = new NotificationHelper(this);
+        Button notif = findViewById(R.id.notif);
         Button logout = (Button) findViewById(R.id.log_out);
         TextView text = (TextView) findViewById(R.id.online_status);
         Integer online = 1;
 
-        logout.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        logout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 try {
                     SharedPreferences data = getSharedPreferences("UserData", MODE_PRIVATE);
                     SharedPreferences.Editor edit = data.edit();
@@ -31,17 +33,32 @@ public class junior_fifth_page_home extends AppCompatActivity {
                     edit.commit();
                     startActivity(new Intent(junior_fifth_page_home.this, first_page.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
                     finish();
-                }catch (Exception e) {
+                } catch (Exception e) {
                     Toast.makeText(getApplicationContext(), "Something Happened", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        if (online==1)
+        if (online == 1)
             text.setText("Current Status: online");
         else
             text.setText("Current Status: offline");
+
+        notif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = getString(R.string.notificationtext);
+                String title = getString(R.string.notificationtitle);
+                Notification.Builder builder = helper.getsfitchannelnotification(title,text);
+                helper.getManager().notify(0,builder.build());
+            }
+        });
+
     }
+
+
+
+
 
     public void onBackPressed() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
