f52fe5720deda7d2d3cd2ccd5200c47c9ef51831
blacklegchef <gavin18d@gmail.com>
2018-03-18 11:28:00 +0530

    location changes

--- app/src/main/java/com/sfit/sahara/mysahara/junior_fifth_page_home.java
+++ app/src/main/java/com/sfit/sahara/mysahara/junior_fifth_page_home.java
@@ -7,13 +7,24 @@ import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class junior_fifth_page_home extends AppCompatActivity {
 
@@ -26,6 +37,8 @@ public class junior_fifth_page_home extends AppCompatActivity {
         Button logout = (Button) findViewById(R.id.log_out);
         TextView text = (TextView) findViewById(R.id.online_status);
         Boolean online = true;
+        final FusedLocationProviderClient locate= LocationServices.getFusedLocationProviderClient(this);
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
 
         logout.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -63,6 +76,50 @@ public class junior_fifth_page_home extends AppCompatActivity {
                 notificationManager.notify(001, builder.build());
             }
         });
+
+        try{
+            final SharedPreferences data = getSharedPreferences("UserData", MODE_PRIVATE);
+            final String username =data.getString("Username",null);
+            /* db.collection("users").document(username).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    slat = (Double) documentSnapshot.get("Current Latitude");
+                    slong = (Double) documentSnapshot.get("Current Longitude");
+
+                }
+            }); */
+            db.collection("users").document(username).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        String s_slat = document.getString("Current Latitude");
+                        String s_slong = document.getString("Current Longitude");
+                        String s_elat = document.getString("Home Longitude");
+                        String s_elong = document.getString("Home Latitude");
+
+                        double slat=0,slong=0,elat=0,elong=0;
+                        float loc;
+
+                        slat=Double.parseDouble(s_slat);
+                        slong=Double.parseDouble(s_slong);
+                        elat=Double.parseDouble(s_elat);
+                        elong=Double.parseDouble(s_elong);
+
+                        Location locationA = new Location("A");
+                        locationA.setLatitude(slat);
+                        locationA.setLongitude(slong);
+                        Location locationB = new Location("B");
+                        locationB.setLatitude(elat);
+                        locationB.setLongitude(elong);
+                        loc=locationA.distanceTo(locationB);
+                        Toast.makeText(getApplicationContext(), (String.valueOf(loc)),Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+            //Location.distanceBetween(slat, slong, elat, elong);
+        }catch (Exception e){}
     }
 
     public void onBackPressed() {
